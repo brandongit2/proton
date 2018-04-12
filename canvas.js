b9bc395434a1294bd34a390b6eab7c50f798c287
Michael Pu <michael.pu123@gmail.com>
2018-04-11 22:42:42 -0400

    Added corner squares to located corners of canvas

--- canvas.js
+++ canvas.js
@@ -1,11 +1,10 @@
 
 const GREY = "#A0A0A0"
-const BLACK = "	#000000";
+const BLACK = "#000000";
+const RED = "#FF0000";
 
 var canvas;
-var cxt2d;
-var canvasWidth;
-var canvasHeight;
+var ctx2d;
 var workspace;
 
 function setUpCanvas() {
@@ -13,10 +12,18 @@ function setUpCanvas() {
     canvas = $("#canvas")[0];
     canvas.height = workspace.getBoundingClientRect().height;
     canvas.width = workspace.getBoundingClientRect().width;
-    cxt2d = canvas.getContext("2d");
-    cxt2d.fillStyle = GREY;
-    cxt2d.fillRect(0,0,canvas.width,canvas.height);
-    cxt2d.fillStyle = BLACK;
-    cxt2d.font="50px Arial";
-    cxt2d.fillText("This is the canvas.", 200, 200);
+    ctx2d = canvas.getContext("2d");
+    
+    ctx2d.fillStyle = GREY;
+    ctx2d.fillRect(0,0,canvas.width,canvas.height);
+    
+    ctx2d.fillStyle = BLACK;
+    ctx2d.font="50px Arial";
+    ctx2d.fillText("This is the canvas.", 200, 200);
+
+    ctx2d.fillStyle = RED;
+    ctx2d.fillRect(0, 0, 5, 5);
+    ctx2d.fillRect(canvas.width-5, 0, 5, 5);
+    ctx2d.fillRect(0, canvas.height-5, 5, 5);
+    ctx2d.fillRect(canvas.width-5, canvas.height-5, 5, 5);
 }
