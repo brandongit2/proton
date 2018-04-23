543665e02bae72c53c49c9e2fecffffcdc999d38
Brandon Tsang <brandononline2@gmail.com>
2018-04-23 12:09:58 -0400

    Workspace is scrollable; goes darker when clicked

--- src/js/workspace.js
+++ src/js/workspace.js
@@ -2,6 +2,7 @@ const SPACE_BETWEEN_DOTS = 50;
 var workspace   = null;
 var totalChange = [0, 0];
 var origin      = null;
+var scale       = 1;
 
 function createWorkspace() {
     workspace = document.getElementById('workspace');
@@ -14,6 +15,8 @@ function createWorkspace() {
 
     var panLoop = null;
     workspace.addEventListener("mousedown", function() {
+        TweenMax.killAll();
+        TweenMax.to("#color-dummy", 0.3, {color: '#9d9d9d'});
         panLoop = setInterval(function() {
             origin.x += deltaX;
             origin.y += deltaY;
@@ -23,19 +26,34 @@ function createWorkspace() {
 
     window.addEventListener("mouseup", function() {
         if (panLoop != null) {
+            TweenMax.killAll();
+            TweenMax.to("#color-dummy", 0.3, {color: '#cfcfcf'});
+            var animateBack = setInterval(function() {
+                renderDots(ctx);
+            }, 10);
+            setTimeout(function() {
+                clearInterval(animateBack);
+            }, 300);
             clearInterval(panLoop);
         }
+    });
+
+    workspace.addEventListener("wheel", function(e) {
+        scale *= e.deltaY > 0 ? 1 / 1.2 : 1.2;
+        renderDots(ctx);
     });
 
     renderDots(ctx);
 }
 
 function renderDots(ctx) {
-    ctx.strokeStyle = '#cfcfcf';
+    ctx.strokeStyle = $('#color-dummy').css('color');
     ctx.clearRect(0, 0, workspace.width, workspace.height);
+    var spaceBetweenDots = SPACE_BETWEEN_DOTS / scale;
+    console.log(spaceBetweenDots);
 
-    for (var i = origin.x % SPACE_BETWEEN_DOTS + 0.5; i < origin.x % SPACE_BETWEEN_DOTS + workspace.width + 0.5; i += SPACE_BETWEEN_DOTS) {
-        for (var j = origin.y % SPACE_BETWEEN_DOTS + 0.5; j < origin.y % SPACE_BETWEEN_DOTS + workspace.width + 0.5; j += SPACE_BETWEEN_DOTS) {
+    for (var i = origin.x % spaceBetweenDots - spaceBetweenDots + 0.5; i < origin.x % spaceBetweenDots + spaceBetweenDots + workspace.width + 0.5; i += spaceBetweenDots) {
+        for (var j = origin.y % spaceBetweenDots - spaceBetweenDots + 0.5; j < origin.y % spaceBetweenDots + spaceBetweenDots + workspace.width + 0.5; j += spaceBetweenDots) {
             ctx.beginPath();
             ctx.moveTo(i - 2, j);
             ctx.lineTo(i + 2, j);
