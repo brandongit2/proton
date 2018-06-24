d4dd7ae0d35689c356d934da2fd56193a04e4d6b
Brandon Tsang <brandononline2@gmail.com>
2018-06-24 13:14:24 -0400

    Created new branch for rewrite.

---
+++ src/js/build.sh
@@ -0,0 +1,4 @@
+for $i in $(*.js); do
+    echo item: $i
+done
+
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+$(function () {
+    ReactDOM.render(Hello, world < /h1>,, document.getElementById('root'));
+});
+;
