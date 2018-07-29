6fe21219b79ce9c061e1f80427ad6d20b4e14cbc
Michael Pu <michael.pu123@gmail.com>
2018-07-29 16:42:15 -0400

    Added PostCSS to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,8 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
+                    'postcss-loader'
                 ],
                 exclude: /node_modules/
             },
